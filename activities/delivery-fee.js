c5198d7123e46fefddc9220cdc59eb1086250ca8
HongYami <97005168+HongYami@users.noreply.github.com>
2022-02-25 18:33:41 +0800

    activities
    activities

---
+++ activities/delivery-fee.js
@@ -0,0 +1,16 @@
+
+// delivery fee
+
+function select(){
+    var selected = document.getElementById("id").value;
+    console.log(selected);
+    }
+
+    function county(){
+        if 
+        }
+
+
+    function add(){
+    document.getElementById('number').value+document.getElementById('number').value+document.getElementById('county').value 
+    }
